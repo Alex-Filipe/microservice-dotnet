db2ab41fc78a2c5d5970019a3ad266ad8093267a
Alex Filipe <alex.silva@rehagro.edu.br>
2024-05-30 21:02:45 -0300

    feature: adcionado validacao de apenas 4 perfis por conta

--- src/UserMicroservice/src/Services/RoleService.cs
+++ src/UserMicroservice/src/Services/RoleService.cs
@@ -24,10 +24,8 @@ namespace UserMicroservice.src.Services
         {
             try
             {
-                if (_roleRepository.GetRoleByName(role.Name) != null)
-                {
-                    throw new Exception("Já existe um perfil com este nome.");
-                }
+                ValidateRoleNameUniqueness(role.Name);
+                ValidateUserRoleLimit(role.User_id);
 
                 CreateRoleDto newRole = new()
                 {
@@ -47,7 +45,7 @@ namespace UserMicroservice.src.Services
         {
             try
             {
-                var role = _roleRepository.GetRoleById(roleId)  ?? throw new Exception("Perfil não encontrado.");
+                var role = _roleRepository.GetRoleById(roleId) ?? throw new Exception("Perfil não encontrado.");
                 return role;
             }
             catch (Exception e)
@@ -65,6 +63,21 @@ namespace UserMicroservice.src.Services
             catch (Exception e)
             {
                 throw new Exception(e.Message);
+            }
+        }
+        private void ValidateRoleNameUniqueness(string roleName)
+        {
+            if (_roleRepository.GetRoleByName(roleName) != null)
+            {
+                throw new Exception("Já existe um perfil com este nome.");
+            }
+        }
+
+        private void ValidateUserRoleLimit(int userId)
+        {
+            if (_roleRepository.GetAllRoles(userId).Count >= 4)
+            {
+                throw new Exception("O usuário já possui 4 perfis.");
             }
         }
     }
