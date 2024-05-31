e2873071c4cd3fcbb8e9ff248ba7fcb9d4c9f088
Alex Filipe <alex.silva@rehagro.edu.br>
2024-05-30 21:18:23 -0300

    fix: correcao do metodo update de perfil

--- src/UserMicroservice/src/DTOs/RoleDTOs/UpdateRoleDto.cs
+++ src/UserMicroservice/src/DTOs/RoleDTOs/UpdateRoleDto.cs
@@ -9,8 +9,5 @@ namespace UserMicroserice.src.Dtos.RoleDTOs
         [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         [StringLength(255, ErrorMessage = "O campo Nome não pode ter mais do que 255 caracteres.")]
         public required string Name { get; set; }
-
-        [Required(ErrorMessage = "O campo user_id é obrigatório.")]
-        public required int User_id { get; set; }
     }
 }
